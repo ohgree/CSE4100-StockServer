e11f633e5339d3ae446f41c1c54272763b48a8d1
MJ Shin <ohgree.dev@gmail.com>
2022-05-18 21:13:59 +0900

    Support parsing for well-formatted commands.
    Note
    ======
     - Parsing only trims right side of received command.
     - Trimming is done on premise that received commands are always formatted
       correctly.
     - Added `misc.h` and `misc.c` headers to maintain non-relevant
       codes.

---
+++ misc.h
@@ -0,0 +1,27 @@
+#ifndef __MISC_H__
+#define __MISC_H__
+
+#include <ctype.h>
+#include <string.h>
+
+#ifdef DEBUG
+#define DEBUG_TEST 1
+#else
+#define DEBUG_TEST 0
+#endif
+
+#define DELIM_CHARS " "
+#define MAX_COMMAND_ARGS 3
+
+#define debug_print(fmt, ...)                                                  \
+  do {                                                                         \
+    if (DEBUG_TEST)                                                            \
+      fprintf(stderr, "%s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__,   \
+              ##__VA_ARGS__);                                                  \
+  } while (0)
+
+char *ltrim(char *s);
+char *rtrim(char *s);
+char *trim(char *s);
+
+#endif /* __MISC_H__ */
